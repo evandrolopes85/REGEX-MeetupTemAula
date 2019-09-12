22ce9450c244f1079f91445b4e56be9ef7623393
evandrolopes85 <evandrocwd@gmail.com>
2019-09-11 22:29:28 -0300

    Adicionando funcao no inicio do padrao

--- meetup/src/dev/evandro/meetup/App.java
+++ meetup/src/dev/evandro/meetup/App.java
@@ -5,7 +5,7 @@ public class App {
 	public static void main(String[] args) {
 		String email =" evandro.lopes85_@yahoo.com.br";
 		
-		String padrao = "[a-z]+([a-z]|[0-9]|[\\Q.-_\\E])*@[a-z]+([a-z]|\\.)*[a-z]{2,3}$";
+		String padrao = "^[a-z]+([a-z]|[0-9]|[\\Q.-_\\E])*@[a-z]+([a-z]|\\.)*[a-z]{2,3}$";
 		
 		System.out.println(email.matches(padrao));
 	}
